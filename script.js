665539df0411a31e768f72f2b171058451da5097
Tanish <110409355+tanish5454@users.noreply.github.com>
2026-01-28 20:32:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function kiss() {
 function nexttomem() {
     document.getElementById("div3").style.display = "none";
     document.getElementById("div2").style.display = "none";
-    document.getElementById("div4").style.display = "block";
+    document.getElementById("div4").style.display = "inline-block";
 }
 
 function memna() {
